bf9f22af1717691878fe33549de0a1329c562a66
Nicolas Castro <pcastro@unrn.edu.ar>
2020-04-24 22:19:51 -0300

    Update credentials

--- detail.php
+++ detail.php
@@ -509,7 +509,7 @@
                                         <?php echo "$" . $_POST['unit'] ?>
                                     </h3>
                                 </div>
-                                <form action="/pay.php" method="POST">
+                                <form action="/procesar-pago" method="POST">
                                     <script
                                             src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
                                             data-preference-id="<?php echo $preference->id; ?>"
