0fb42b1df29266bd076714024677b2f43263bcfd
Nicolas Castro <pcastro@unrn.edu.ar>
2020-04-24 22:47:29 -0300

    Update credentials

--- notification.php
+++ notification.php
@@ -20,4 +20,9 @@ switch($_POST["type"]) {
 $content = json_encode($_POST);
 file_put_contents('log.txt', $content);
 
+$data = file_get_contents('php://input');
+$f = fopen('raw.txt', 'a');
+fwrite($f, $data);
+fclose($f);
+
 ?>
